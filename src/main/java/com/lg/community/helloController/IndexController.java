ea7c5b3670533d6edcb10e50d0e2b149ba2a98b5
longgang <1063824776@qq.com>
2019-07-26 03:40:42 +0800

    add login's cookie and session

--- src/main/java/com/lg/community/helloController/IndexController.java
+++ src/main/java/com/lg/community/helloController/IndexController.java
@@ -1,13 +1,37 @@
 package com.lg.community.helloController;
 
+import com.lg.community.mapper.UserMapper;
+import com.lg.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
 @Controller
 public class IndexController {
+    @Autowired
+    UserMapper userMapper;
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if(cookies == null){
+            return "index";
+        }
+        for (Cookie cookie:
+            cookies ) {
+            if(cookie.getName().equals("token")){
+                User  user = userMapper.findUserByToken(cookie.getValue());
+                if (user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
         return "index";
     }
 
--- src/main/java/com/lg/community/mapper/UserMapper.java
+++ src/main/java/com/lg/community/mapper/UserMapper.java
@@ -3,9 +3,14 @@ package com.lg.community.mapper;
 import com.lg.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user(account_id,name,token,gmt_create,gmt_modified) values (#{accountId},#{name},#{token},#{gmtCreat},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findUserByToken(@Param("token") String token);
 }
